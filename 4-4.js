8831eda2bedc71a8ab22e0eb185d20f9a1e6e336
Taemin Kim <taemin3000@naver.com>
2017-10-28 17:04:56 +0900

    readline modules

--- 4-4.js
+++ 4-4.js
@@ -1,2 +1,9 @@
 var readline = require("readline");
-var r = readline.createInerface
+var r = readline.createInerface(
+    {input:process.stdin, output:process.stdout}
+)
+
+r.question("이름은 ? ", function(name){
+    console.log("당신의 이름은 " + name + "dlqslek.");
+    r.close(); // 공통자원을 사용하므로 반드시 close 해야함.
+})
