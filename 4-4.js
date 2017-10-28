2e8aebe718350dfe87a7184425c08a7f1e37a16c
Taemin Kim <taemin3000@naver.com>
2017-10-28 17:24:21 +0900

    modify readline file

--- 4-4.js
+++ 4-4.js
@@ -1,9 +1,10 @@
 var readline = require("readline");
-var r = readline.createInerface(
+
+var r = readline.createInterface(
     {input:process.stdin, output:process.stdout}
 )
 
 r.question("이름은 ? ", function(name){
-    console.log("당신의 이름은 " + name + "dlqslek.");
+    console.log("당신의 이름은 " + name + "입니다.");
     r.close(); // 공통자원을 사용하므로 반드시 close 해야함.
 })
